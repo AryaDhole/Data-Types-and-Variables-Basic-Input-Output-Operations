ec1f0635b375acaf31c46ae3c6901a3dfb07a798
Arya Dhole <114810037+AryaDhole@users.noreply.github.com>
2023-09-26 20:10:03 +0530

    Add files via upload

---
+++ Size Of Variables.cpp
@@ -0,0 +1,18 @@
+//Arya Dhole
+//ENTC-A2
+//22070123027
+
+#include <iostream>
+using namespace std;
+int main()
+{
+    string myStr = "Arya";
+    char myChar= 'A';
+    int myInt = 21;
+    float myFloat = 94.5;
+    cout<< sizeof(myStr) << endl ;
+    cout<< sizeof(myChar) << endl ;
+    cout<< sizeof(myInt) << endl  ;
+    cout<< sizeof(myFloat);
+    return 0;
+}
